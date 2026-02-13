2eec3857a95fcd0bde0cfd94334eff74f40eccc0
xskutsu <xsku@protonmail.com>
2026-02-13 06:47:04 -0600

    Remove unused import

--- src/main/java/sereneseasons/network/message/MessageSyncSeasonCycle.java
+++ src/main/java/sereneseasons/network/message/MessageSyncSeasonCycle.java
@@ -13,7 +13,6 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.relauncher.Side;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
-import sereneseasons.handler.season.SeasonHandler;
 
 public class MessageSyncSeasonCycle implements IMessage, IMessageHandler<MessageSyncSeasonCycle, IMessage>
 {
